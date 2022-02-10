722b3bb5aca0742522c63c2cd697f265f6e90dd4
harini1708 <73392962+harini1708@users.noreply.github.com>
2022-02-10 19:32:51 +0530

    Delete main.c

--- 3_Implementation/src/main.c
+++
@@ -1,8 +0,0 @@
-#include"Sample.h"
-    void main()
-{
-    printf("\n*******Welcome to General Knowledge Quiz Game*******\n");
-    printf("Please Enter your name:");
-    gets(name);
-    menu();
-}
