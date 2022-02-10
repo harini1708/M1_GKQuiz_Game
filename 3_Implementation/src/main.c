a54d4d66ae547567c36299f3585c9bef4feac536
harini1708 <harinis1708@gmail.com>
2022-02-10 18:44:37 +0530

    Added files to Implementation,#1

---
+++ 3_Implementation/src/main.c
@@ -0,0 +1,8 @@
+#include"Sample.h"
+    void main()
+{
+    printf("\n*******Welcome to General Knowledge Quiz Game*******\n");
+    printf("Please Enter your name:");
+    gets(name);
+    menu();
+}
